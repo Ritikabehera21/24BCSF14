feb61f9084c9a4d4aeec9d54fcecdcf768df585b
Ritikabehera21 <Cse.24bcsf14@silicon.ac.in>
2025-09-13 18:47:55 +0530

    Create q3.java

---
+++ JAVA/ASSIGNMENT1/q3.java
@@ -0,0 +1,13 @@
+//  Q3. Write a program to input two floating point numbers through the keyboard and display their sum.
+
+import java.util.Scanner;
+class Sum {
+    public static void main(String s[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter two floating point numbers: ");
+        double x = sc.nextDouble();
+        double y = sc.nextDouble();
+        double res = x + y;
+        System.out.println("The sum of " + x + " and " + y + " is " + res);
+    }
+}
