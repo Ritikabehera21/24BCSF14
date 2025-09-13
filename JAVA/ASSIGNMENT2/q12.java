57bc281d4374dab9f065f16dd32c1f42be31e7c2
Ritikabehera21 <Cse.24bcsf14@silicon.ac.in>
2025-09-13 19:24:44 +0530

    Create q12.java

---
+++ JAVA/ASSIGNMENT2/q12.java
@@ -0,0 +1,18 @@
+//	Q12. Write a recursive program to find the sum of n natural numbers.
+
+import java.util.Scanner;
+class RecursiveSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        System.out.println("The sum of first " + n + " natural numbers is " + sum(n));
+    }
+
+    private static int sum(int n) {
+        if (n == 0) {
+            return n;
+        }
+        return n + sum(n - 1);
+    }
+}
