20b00b88bd5b98c5b2fd14aad0d20e3dd5961d9e
Ritikabehera21 <Cse.24bcsf14@silicon.ac.in>
2025-09-13 19:00:10 +0530

    Create q11.java

---
+++ JAVA/ASSIGNMENT1/q11.java
@@ -0,0 +1,16 @@
+//  Q11. Write a program to check whether an inputted number is positive or negative.
+
+import java.util.Scanner;
+class Positive {
+    public static void main(String s[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (num < 0) {
+            System.out.println(num + " is negative number");
+        } else {
+            System.out.println(num + " is positive number");
+        }
+    }
+}
