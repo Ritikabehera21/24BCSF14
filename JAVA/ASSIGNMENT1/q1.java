046681237efe431df667e8319f90e618d30f05e6
Ritikabehera21 <Cse.24bcsf14@silicon.ac.in>
2025-09-13 18:45:09 +0530

    Create q1.java

---
+++ JAVA/ASSIGNMENT1/q1.java
@@ -0,0 +1,10 @@
+//  Q1. Write a program to display your name, branch, roll no, and college name on the computer screen.
+
+class Info {
+    public static void main(String s[]) {
+        System.out.println("Name: Ritika Behera");
+        System.out.println("Branch: CSE");
+        System.out.println("Roll No: 15");
+        System.out.println("College: Silicon University");
+    }
+}
